ea086aa4ca5059d6dacf733edcf3b910386a1e42
Simone Taeggi <startae14@gmail.com>
2020-01-28 23:50:14 +0100

    added Snapshot testing

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,8 +1,8 @@
-import { shallow, render, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import Card from './Card';
 
 it('expect to render Card component', () => {
-  console.log(shallow(<Card />));
-  expect(shallow(<Card />).length).toEqual(1);
+  console.log(shallow(<Card />).debug());
+  expect(shallow(<Card />)).toMatchSnapshot();
 })
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,5 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16.2';
+import "jest-enzyme";
 
-configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
