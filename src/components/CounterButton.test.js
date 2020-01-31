996f0dfd4d619c8d129cceeab0b2fd68204f2f7c
Simone Taeggi <startae14@gmail.com>
2020-01-31 22:40:16 +0100

    test a state component shouldComponentUpdate and setState

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -3,5 +3,19 @@ import React from 'react';
 import CounterButton from './CounterButton';
 
 it('expect to render CounterButton component', () => {
-  expect(shallow(<CounterButton />)).toMatchSnapshot();
+  const wrapper = shallow(<CounterButton />);
+  expect(wrapper).toMatchSnapshot();
+  const instance = wrapper.instance();
+  //Accessing component state
+  expect(instance.state.count).toBe(1);
+  // test the shouldComponentUpdate lifecycle method
+  const shouldUpdate = instance.shouldComponentUpdate({}, { count: 1});
+  // if the state is not changed
+  expect(shouldUpdate).toBe(false);
+  // click on button and change the counter
+  wrapper.find('button').props().onClick();
+  expect(wrapper.state()).toEqual({ count: 2 });
+  // if the state is changed
+  const shouldUpdate2 = instance.shouldComponentUpdate({}, { count: 1});
+  expect(shouldUpdate2).toBe(true);
 })
