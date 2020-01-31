30bed8e1ccc6c2d4546d14168241f854929b98c3
Simone Taeggi <startae14@gmail.com>
2020-01-31 22:58:32 +0100

    test stateful components

--- src/components/ErrorBoundry.test.js
+++ src/components/ErrorBoundry.test.js
@@ -3,5 +3,15 @@ import React from 'react';
 import ErrorBoundry from './ErrorBoundry';
 
 it('expect to render ErrorBoundry component', () => {
-  expect(shallow(<ErrorBoundry />)).toMatchSnapshot();
+  const wrapper = shallow(<ErrorBoundry />);
+  expect(wrapper).toMatchSnapshot();
+  const instance = wrapper.instance();
+  // test the state value
+  expect(instance.state.hasError).toBe(false);
+  // force change state
+  wrapper.setState({ hasError: true });
+  // test the componentDidCatch
+  instance.componentDidCatch('oh nooos an error', 'it is an error');
+  // test the state value
+  expect(instance.state.hasError).toBe(true);
 })
--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -3,5 +3,11 @@ import React from 'react';
 import Header from './Header';
 
 it('expect to render Header component', () => {
-  expect(shallow(<Header />)).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
+  // test should component update
+  const instance = wrapper.instance();
+  // test the shouldComponentUpdate lifecycle method
+  const shouldUpdate = instance.shouldComponentUpdate({}, {});
+  expect(shouldUpdate).toBe(false);
 })
