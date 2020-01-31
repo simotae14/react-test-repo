390fb938d25548d3c7cd11fe65067b0564dd49de
Simone Taeggi <startae14@gmail.com>
2020-01-31 21:55:37 +0100

    snapshot tests

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -3,6 +3,5 @@ import React from 'react';
 import Card from './Card';
 
 it('expect to render Card component', () => {
-  console.log(shallow(<Card />).debug());
   expect(shallow(<Card />)).toMatchSnapshot();
 })
