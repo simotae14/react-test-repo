35562b67f90d04926c238b781540e45c103e4c2d
Simone Taeggi <startae14@gmail.com>
2020-02-05 01:19:42 +0100

    testing connected components

---
+++ src/components/MainPage.test.js
@@ -0,0 +1,39 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import MainPage from './MainPage';
+
+let wrapper;
+beforeEach(() => {
+  const mockProps = {
+    onRequestRobots: jest.fn(),
+    robots: [],
+    searchField: '',
+    isPending: false
+  };
+  wrapper = shallow(<MainPage {...mockProps} />)
+});
+
+describe('test the MainPage component', () => {
+  it('renders MainPage without crashing', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('filters robots correctly', () => {
+    const mockProps2 = {
+      onRequestRobots: jest.fn(),
+      robots: [{
+        id: 3,
+        name: 'John',
+        email: 'john@gmail.com'
+      }],
+      searchField: 'john',
+      isPending: false
+    };
+    wrapper2 = shallow(<MainPage {...mockProps2} />)
+    expect(wrapper2.instance().filterRobots()).toEqual([{
+      id: 3,
+      name: 'John',
+      email: 'john@gmail.com'
+    }]);
+  });
+});
