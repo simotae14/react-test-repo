cf771ca2ce004e8e38295370518e68d363438953
Simone Taeggi <startae14@gmail.com>
2020-02-09 23:26:32 +0100

    testing connected components

--- src/components/MainPage.test.js
+++ src/components/MainPage.test.js
@@ -29,11 +29,43 @@ describe('test the MainPage component', () => {
       searchField: 'john',
       isPending: false
     };
-    wrapper2 = shallow(<MainPage {...mockProps2} />)
+    const wrapper2 = shallow(<MainPage {...mockProps2} />)
     expect(wrapper2.instance().filterRobots()).toEqual([{
       id: 3,
       name: 'John',
       email: 'john@gmail.com'
     }]);
+  });
+
+  it('filters robots correctly 2', () => {
+    const mockProps3 = {
+      onRequestRobots: jest.fn(),
+      robots: [{
+        id: 3,
+        name: 'John',
+        email: 'john@gmail.com'
+      }],
+      searchField: 'a',
+      isPending: false
+    };
+    const filteredRobots = [];
+    const wrapper3 = shallow(<MainPage {...mockProps3} />)
+    expect(wrapper3.instance().filterRobots()).toEqual(filteredRobots);
+  });
+
+  it('filters robots correctly 3, with is Pending', () => {
+    const mockProps4 = {
+      onRequestRobots: jest.fn(),
+      robots: [{
+        id: 3,
+        name: 'John',
+        email: 'john@gmail.com'
+      }],
+      searchField: 'a',
+      isPending: true
+    };
+    const filteredRobots = [];
+    const wrapper4 = shallow(<MainPage {...mockProps4} />)
+    expect(wrapper4.instance().filterRobots()).toEqual(filteredRobots);
   });
 });
